8d8098913459814a1787fda30004fd0f7d076828
Goose <64352864+Goose-Of-War@users.noreply.github.com>
2023-02-20 18:11:10 +0530

    Login middleware fixes

--- src/login.js
+++ src/login.js
@@ -2,7 +2,8 @@ const dbh = require('../database/handler');
 
 module.exports = async (req, res, next) => {
 	// Basically, I'll use this to generate and manage sessions, while also adding req.user
-	const { sessionId } = req.cookies;
-	if (!sessionId) return;
+	const sessionId = req.cookies.sessionId;
+	if (!sessionId) return next();
 	req.user = await dbh.returnUserFromSession(sessionId);
+	next();
 };
