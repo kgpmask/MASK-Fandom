0f692988b3a11c3ae2da4462d4de970568259e0b
Goose <64352864+Goose-Of-War@users.noreply.github.com>
2023-03-05 15:58:45 +0530

    email in user schema

--- database/schemas/User.js
+++ database/schemas/User.js
@@ -4,6 +4,7 @@ const userInfoSchema = new mongoose.Schema({
 	_id: { type: String, required: true },
 	name: { type: String, required: true },
 	username: { type: String, required: true },
+	email: { type: String, required: true },
 	salt: { type: String, required: true },
 	hash: { type: String, required: true },
 	signedUpFor: {
